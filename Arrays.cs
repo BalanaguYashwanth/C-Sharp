59b175e7d1c742732ca562cc683a776d41c958b6
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-06-14 17:28:42 +0530

    Create Arrays.cs

---
+++ Arrays.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Arrays
+{
+    class Numbers
+    {
+       static void Main(string[] args)
+        {
+            var name = new string[3];
+            name[0] = "yash";
+            Console.WriteLine(name[0]);
+            Console.WriteLine("name[1] is ",name[1]);
+            Console.WriteLine("name[2] is ", name[2]);
+        }        
+    }
+}
