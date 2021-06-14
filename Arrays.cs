834c841f7eedaf3ca454aa857636db460a24ad3d
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-06-14 17:45:15 +0530

    Update Arrays.cs

--- Arrays.cs
+++ Arrays.cs
@@ -10,6 +10,7 @@ namespace Arrays
             Console.WriteLine(name[0]);
             Console.WriteLine(name[1]);
             Console.WriteLine(name[2]);
+            var fullname = String.Format("My name is {0} {1} {2}", name[0],name[1],name[2]);
         }        
     }
 }
