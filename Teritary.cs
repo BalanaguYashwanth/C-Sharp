f9b1de711f9215c7ed5ba8967e978f6189c305ac
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-06-20 13:12:47 +0530

    Create Teritary.cs

---
+++ Teritary.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isGold = false;
+
+            float value = isGold ? 19.34f : 22.4f;
+
+            Console.WriteLine(value);
+        }
+    }
+}
