53813f52a1057346051fd7bc8dccac551203239d
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-06-20 14:24:22 +0530

    Create Switch.cs

---
+++ Switch.cs
@@ -0,0 +1,32 @@
+using System;
+using ControlFlow;
+
+namespace Operations
+{
+    public enum Season
+    {
+        Winter,
+        Summer,
+        Antum,
+        Spring
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season.Summer;
+            switch(season)
+            {
+                case Season.Antum:
+                    Console.WriteLine("go to work");
+                    break;
+                case Season.Spring:
+                    Console.WriteLine("spring is came");
+                    break;
+                default:
+                    Console.WriteLine("No to work");
+                    break;
+            }
+        }
+    }
+}
