9fc7b839e2af1b17ca4a81f9222cd3669bb03ea4
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-06-20 15:51:04 +0530

    Create Forloop.cs

---
+++ Forloop.cs
@@ -0,0 +1,16 @@
+using System;
+using ControlFlow;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(var i=10;i>1;i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
